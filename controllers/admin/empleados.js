17b582a17640b05620daa10ba5112d4b83699bca
Enrique2442 <20220481@ricaldone.edu.sv>
2024-05-13 13:56:31 -0600

    empleados

--- controllers/admin/empleados.js
+++ controllers/admin/empleados.js
@@ -32,6 +32,8 @@ vanillaTextMask.maskInput({
 
 // Método del evento para cuando el documento ha cargado.
 document.addEventListener('DOMContentLoaded', () => {
+
+    loadTemplate5();
     // Llamada a la función para llenar la tabla con los registros existentes.
     fillTable();
 });
