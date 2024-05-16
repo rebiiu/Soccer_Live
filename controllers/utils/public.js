6f31e7b657b34c3cedb41afc23436f3fd819cbab
stanl <20220383@ricaldone.edu.sv>
2024-05-15 23:33:59 -0600

    Cliente sesion

--- controllers/utils/public.js
+++ controllers/utils/public.js
@@ -9,8 +9,13 @@ const USER_API = 'services/public/cliente.php';
 const MAIN = document.querySelector('main');
 MAIN.style.paddingTop = '0px';
 MAIN.style.paddingBottom = '0px';
-const MAIN_TITLE = document.getElementById('mainTitle');
-MAIN.classList.remove('container');
+MAIN.classList.add('container');
+// Se establece el título de la página web.
+// document.querySelector('title').textContent = 'CoffeeShop - Store';
+// Constante para establecer el elemento del título principal.
+// const MAIN_TITLE = document.getElementById('mainTitle');
+// MAIN_TITLE.classList.add('text-center', 'py-3');
+
 /*  Función asíncrona para cargar el encabezado y pie del documento.
 *   Parámetros: ninguno.
 *   Retorno: ninguno.
@@ -24,64 +29,56 @@ const loadTemplate = async () => {
         if (!location.pathname.endsWith('login.html')) {
             // Se agrega el encabezado de la página web antes del contenido principal.
             MAIN.insertAdjacentHTML('beforebegin', `
-                <header>
-                    <nav class="navbar fixed-top navbar-expand-lg bg-body-tertiary">
-                        <div class="container">
-                            <a class="navbar-brand" href="index.html"><img src="../../resources/img/logo.png" height="50" alt="CoffeeShop"></a>
-                            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                                <span class="navbar-toggler-icon"></span>
-                            </button>
-                            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                                <div class="navbar-nav ms-auto">
-                                    <a class="nav-link" href="index.html"><i class="bi bi-shop"></i> Catálogo</a>
-                                    <a class="nav-link" href="cart.html"><i class="bi bi-cart"></i> Carrito</a>
-                                    <a class="nav-link" href="#" onclick="logOut()"><i class="bi bi-box-arrow-left"></i> Cerrar sesión</a>
-                                </div>
-                            </div>
-                        </div>
-                    </nav>
-                </header>
+            <br>
+            <!-- Contenido de menú del documento-->
+        <nav class="navbar navbar-expand-lg navbar-dark static-top" id="fondo">
+            <div class="container">
+                <!-- Se llama a la imagen -->
+                <a class="navbar-brand" href="../public/index.html">
+                    <img src="../../resources/imgs/logo-no-background.png" alt="..." height="36">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <!-- Menú -->
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="../public/index.html" id="inicio">INICIO</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="../public/balones.html">BALONES</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="../public/guantes.html">GUANTES</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="../public/botellas.html">BOTELLAS</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="../public/calzado.html">CALZADO</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="../public/info.html">¿QUIÉNES SOMOS?</a>
+                        </li>
+                        <li class="nav-item">
+                        <div class="navbar-nav ms-auto">
+                        <a class="nav-link" href="cart.html"><i class="bi bi-cart"></i> Carrito</a>
+                        <a class="nav-link" href="#" onclick="logOut2()"><i class="bi bi-box-arrow-left"></i> Cerrar sesión</a>
+                    </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
             `);
         } else {
             location.href = 'index.html';
         }
     } else {
-        // Se agrega el encabezado de la página web antes del contenido principal.
-        MAIN.insertAdjacentHTML('beforebegin', `
-            <header>
-                <nav class="navbar fixed-top navbar-expand-lg bg-body-tertiary">
-                    <div class="container">
-                        <a class="navbar-brand" href="index.html"><img src="../../resources/img/logo.png" height="50" alt="CoffeeShop"></a>
-                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
-                        </button>
-                        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                            <div class="navbar-nav ms-auto">
-                                <a class="nav-link" href="index.html"><i class="bi bi-shop"></i> Catálogo</a>
-                                <a class="nav-link" href="signup.html"><i class="bi bi-person"></i> Crear cuenta</a>
-                                <a class="nav-link" href="login.html"><i class="bi bi-box-arrow-right"></i> Iniciar sesión</a>
-                            </div>
-                        </div>
-                    </div>
-                </nav>
-            </header>
-        `);
+        sweetAlert(3, DATA.error, false, 'index.html');
+
     }
-    // Se agrega el pie de la página web después del contenido principal.
-    MAIN.insertAdjacentHTML('afterend', `
-        <footer>
-            <nav class="navbar fixed-bottom bg-body-tertiary">
-                <div class="container">
-                    <div>
-                        <h6>CoffeeShop</h6>
-                        <p><i class="bi bi-c-square"></i> 2018-2024 Todos los derechos reservados</p>
-                    </div>
-                    <div>
-                        <h6>Contáctanos</h6>
-                        <p><i class="bi bi-envelope"></i> dacasoft@outlook.com</p>
-                    </div>
-                </div>
-            </nav>
-        </footer>
-    `);
 }
